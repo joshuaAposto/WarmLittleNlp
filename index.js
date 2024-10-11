e8bc8f22b5140287566835f8625ed586a06a56f4
Coco martin <150522458+joshuaAposto@users.noreply.github.com>
2024-10-11 11:05:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'))
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 //simisimi v1
 app.get('/simisimi', async (req, res) => {
   const { prompt } = req.query;
